ce618bab3599dde3a4c81c6da9736053824b566c
MohamedAbdelhamed0 <dedo.abdelhamed@gmail.com>
2024-02-09 04:39:26 +0200

    Deploy to gh-pages

--- android/app/src/main/kotlin/com/example/realtykingdom/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.realtykingdom
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/assets_path.dart
+++
@@ -1,4 +0,0 @@
-class PathSvg {
-  static String logo = 'assets/svg/logo/logo.png';
-  static String logoPng = 'assets/svg/logo/logo.png';
-}
